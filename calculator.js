088267173bef4fe85a7e85c4feabb15545b5b99c
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-04-12 16:18:19 +0200

    multiply error

--- calculator.js
+++ calculator.js
@@ -9,11 +9,15 @@ function subtract(a, b = 0) {
 }
 
 function divide(a, b) {
+if(!a && !b) return 0;
+
  return a / b;
 }
 
 function multiply(a, b) {
+  if(!a && !b) return 0;
   return a * b;
+
 }
 
 // The following is required to make unit tests work. Please ignore it.
